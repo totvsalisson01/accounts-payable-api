6a37ad8b612282071e9b3eede15a61bc213aaaf0
Alisson Lino <totvsalisson01@gmail.com>
2025-02-11 08:32:07 -0300

    chore: initial project setup with Spring Boot and core dependencies

---
+++ src/main/java/com/totvs/alisson/contas_pagar_api/ContasAPagarApiApplication.java
@@ -0,0 +1,13 @@
+package com.totvs.alisson.contas_pagar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasAPagarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasAPagarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totvs/alisson/contas_pagar_api/ContasAPagarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totvs.alisson.contas_pagar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasAPagarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
